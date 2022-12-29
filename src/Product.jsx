7f0f15940e56862d92fd9b5d3ad028887684cf30
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2022-12-29 13:21:24 +0530

    data

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,92 +1,69 @@
 import React from "react";
 
 export default function Product() {
-  {
-  "items": [
+  let items = [
     {
-      "sys": { "id": "1" },
-      "fields": {
-        "title": "queen panel bed",
-        "type": "Household",
-        "price": 2500,
-        "rating": 4,
-        "image": { "fields": { "file": { "url": "./images/product-1.jpeg" } } }
-      }
+      title: "queen panel bed",
+      type: "Household",
+      price: 2500,
+      rating: 4,
+      image: { fields: { file: { url: "./images/product-1.jpeg" } } }
+    },
+
+    {
+      title: "king panel bed",
+      type: "Household",
+      price: 3000,
+      rating: 3,
+      image: { fields: { file: { url: "./images/product-2.jpeg" } } }
     },
     {
-      "sys": { "id": "2" },
-      "fields": {
-        "title": "king panel bed",
-        "type": "Household",
-        "price": 3000,
-        "rating": 3,
-        "image": { "fields": { "file": { "url": "./images/product-2.jpeg" } } }
-      }
+      title: "single panel bed",
+      type: "Household",
+      price: 2200,
+      rating: 5,
+      image: { fields: { file: { url: "./images/product-3.jpeg" } } }
     },
     {
-      "sys": { "id": "3" },
-      "fields": {
-        "title": "single panel bed",
-        "type": "Household",
-        "price": 2200,
-        "rating": 5,
-        "image": { "fields": { "file": { "url": "./images/product-3.jpeg" } } }
-      }
+      title: "twin panel bed",
+      type: "Household",
+      price: 2800,
+      rating: 4,
+      image: { fields: { file: { url: "./images/product-4.jpeg" } } }
     },
     {
-      "sys": { "id": "4" },
-      "fields": {
-        "title": "twin panel bed",
-        "type": "Household",
-        "price": 2800,
-        "rating": 4,
-        "image": { "fields": { "file": { "url": "./images/product-4.jpeg" } } }
-      }
+      title: "fridge",
+      type: "Kitchen",
+      price: 1880,
+      rating: 2,
+      image: { fields: { file: { url: "./images/product-5.jpeg" } } }
     },
     {
-      "sys": { "id": "5" },
-      "fields": {
-        "title": "fridge",
-        "type": "Kitchen",
-        "price": 1880,
-        "rating": 2,
-        "image": { "fields": { "file": { "url": "./images/product-5.jpeg" } } }
-      }
+      title: "dresser",
+      type: "furniture",
+      price: 3200,
+      rating: 3,
+      image: { fields: { file: { url: "./images/product-6.jpeg" } } }
     },
     {
-      "sys": { "id": "6" },
-      "fields": {
-        "title": "dresser",
-        "type": "furniture",
-        "price": 3200,
-        "rating": 3,
-        "image": { "fields": { "file": { "url": "./images/product-6.jpeg" } } }
-      }
+      title: "Coombes",
+      type: "Lounge",
+      price: 4500,
+      rating: 5,
+      image: { fields: { file: { url: "./images/product-7.jpeg" } } }
     },
     {
-      "sys": { "id": "7" },
-      "fields": {
-        "title": "Coombes",
-        "type": "Lounge",
-        "price": 4500,
-        "rating": 5,
-        "image": { "fields": { "file": { "url": "./images/product-7.jpeg" } } }
-      }
-    },
-    {
-      "sys": { "id": "8" },
-      "fields": {
-        "title": "Keeve Set",
-        "type": "Tables & Chairs",
-        "price": 1300,
-        "rating": 4,
-        "image": { "fields": { "file": { "url": "./images/product-8.jpeg" } } }
-      }
+      title: "Keeve Set",
+      type: "Tables & Chairs",
+      price: 1300,
+      rating: 4,
+      image: { fields: { file: { url: "./images/product-8.jpeg" } } }
     }
-  ]
-}
-  
-  
-  
-  return <></>;
+  ];
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
