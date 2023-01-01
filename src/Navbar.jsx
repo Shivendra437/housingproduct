8c6330e36a15882fbda3064bde3fee2b3d4c305e
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-01 11:50:11 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
 export default function Navbar() {
-  return <></>;
+  return <>
+    
+    
+     <nav class="navbar">
+      <div class="navbar-center">
+            <span class="nav-icon">
+                <i class="fas fa-bars"></i>
+            </span>
+            <ul>
+                <li><a href="#Home">HOME</a></li>
+                <li><a href="#Shop">SHOP</a></li>
+                <li><a href="#Magazine">MAGAZINE</a></li>
+            </ul>
+            <div class="searchbar">
+                <span><i class="fa-solid fa-magnifying-glass"></i></span>
+                <input type="text">
+                
+                <button class="search">search</button>
+            </div>
+            <div class="cart-btn">
+                <span ><i class="fas fa-cart-plus"></i></span></li>
+                <div class="cart-items">0</div>
+            </div>
+            <a id="myBtn" class="login-link">LOGIN</a>
+      </div>
+    </nav>
+    
+    
+    
+    </>;
 }
