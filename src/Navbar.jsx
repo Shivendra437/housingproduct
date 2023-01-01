f5040091bd7c43b8bdf89dabf64a1f92ba11df40
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-01 12:07:29 +0530

    navabr created

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,34 +1,21 @@
 import React from "react";
 
 export default function Navbar() {
-  return <>
-    
-    
-     <nav class="navbar">
-      <div class="navbar-center">
-            <span class="nav-icon">
-                <i class="fas fa-bars"></i>
-            </span>
-            <ul>
-                <li><a href="#Home">HOME</a></li>
-                <li><a href="#Shop">SHOP</a></li>
-                <li><a href="#Magazine">MAGAZINE</a></li>
-            </ul>
-            <div class="searchbar">
-                <span><i class="fa-solid fa-magnifying-glass"></i></span>
-                <input type="text">
-                
-                <button class="search">search</button>
-            </div>
-            <div class="cart-btn">
-                <span ><i class="fas fa-cart-plus"></i></span></li>
-                <div class="cart-items">0</div>
-            </div>
-            <a id="myBtn" class="login-link">LOGIN</a>
+  return (
+    <>
+      <div className="navabr">
+        <ul>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>Magazine</li>
+        </ul>
+        <div className="Searchbar">
+          <input type="text" />
+          <input type="submit" className="btn" />
+        </div>
+        <div className="cart">Cart</div>
+        <div className="login">Login</div>
       </div>
-    </nav>
-    
-    
-    
-    </>;
+    </>
+  );
 }
